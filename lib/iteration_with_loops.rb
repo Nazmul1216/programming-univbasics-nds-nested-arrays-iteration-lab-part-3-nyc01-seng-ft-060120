6f4304114724956cddbff3a86e4a07534cf1c39d
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-13 23:50:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,6 +5,10 @@ string=
   row_index=0 
   while row_index<src.count do 
     element_index=0 
-    while element_index<src[row_index]
+    while element_index<src[row_index].count
+    p src[row_index][element_index]
+    element_index+=1
+  end
+  row_index+=1
     
 end
